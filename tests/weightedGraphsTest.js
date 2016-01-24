2c54c495cef9c1d31bfe631ecc1c821b194b2048
Srijayanth Sridhar <srijayanth@gmail.com>
2016-01-24 19:50:19 +0530

    Refactored tests to have edge be a new object that also has an id associated with it

--- tests/weightedGraphsTest.js
+++ tests/weightedGraphsTest.js
@@ -9,11 +9,12 @@ describe("shortest path",function(){
 		g.addVertex('A');
 		g.addVertex('B');
 
-		g.addEdge('A','B',1);
+		var e1=new graphs.Edge("e1",'A','B',1);
+		g.addEdge(e1);
 
 		var path=g.shortestPath('A','B');
 		assert.equal(1,path.length);
-		assert.deepEqual(path[0],{from:'A',to:'B',weight:1});
+		assert.deepEqual(e1,path[0]);
 	});
 
 	it("should choose the path with least weight when more than one path exists",function(){
@@ -22,13 +23,16 @@ describe("shortest path",function(){
 		g.addVertex('B');
 		g.addVertex('C');
 
-		g.addEdge('A','B',1);
-		g.addEdge('B','C',1);
-		g.addEdge('A','C',1);
+		var e1=new graphs.Edge("e1",'A','B',1);
+		var e2=new graphs.Edge("e2",'B','C',1);
+		var e3=new graphs.Edge("e1",'A','C',1);
+		g.addEdge(e1);
+		g.addEdge(e2);
+		g.addEdge(e3);
 
 		var path=g.shortestPath('A','C');
 		assert.equal(1,path.length);
-		assert.deepEqual(path[0],{from:'A',to:'C',weight:1});
+		assert.deepEqual(e3,path[0]);
 	});
 
 	it("should choose the path with least weight when more than one path exists even if the path has more vertices",function(){
@@ -37,14 +41,17 @@ describe("shortest path",function(){
 		g.addVertex('B');
 		g.addVertex('C');
 
-		g.addEdge('A','B',1);
-		g.addEdge('B','C',1);
-		g.addEdge('A','C',3);
+		var e1=new graphs.Edge("e1",'A','B',1);
+		var e2=new graphs.Edge("e2",'B','C',1);
+		var e3=new graphs.Edge("e1",'A','C',3);
+		g.addEdge(e1);
+		g.addEdge(e2);
+		g.addEdge(e3);
 
 		var path=g.shortestPath('A','C');
 		assert.equal(2,path.length);
-		assert.deepEqual(path[0],{from:'A',to:'B',weight:1});
-		assert.deepEqual(path[1],{from:'B',to:'C',weight:1});
+		assert.deepEqual(e1,path[0]);
+		assert.deepEqual(e2,path[1]);
 	});
 
 	it("should choose the path with least weight when multiple edges exist between two vertices",function(){
@@ -52,12 +59,14 @@ describe("shortest path",function(){
 		g.addVertex('A');
 		g.addVertex('B');
 
-		g.addEdge('A','B',1);
-		g.addEdge('A','B',2);
+		var e1=new graphs.Edge("e1",'A','B',1);
+		var e2=new graphs.Edge("e2",'A','B',2);
+		g.addEdge(e1);
+		g.addEdge(e2);
 
 		var path=g.shortestPath('A','B');
 		assert.equal(1,path.length);
-		assert.deepEqual(path[0],{from:'A',to:'B',weight:1});
+		assert.deepEqual(e1,path[0]);
 	});
 
 });
